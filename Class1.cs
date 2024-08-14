ed9937f20e8af7114facda297011fbd8386720c5
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-08-14 12:11:21 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Class1
+    {
+        //method1 takes in two integers
+        public static int method1(int x, int y)
+        {
+            int Add = x + 5;
+            int Add2 = y + Add;  return Add2;
+        }
+        public static int method2(int a, int b)
+        {
+            int Subtract = a - 1;
+            int Subtract2 = b - Subtract; return Subtract2;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //method 1
+            Console.WriteLine("Enter either one or two numbers to be added to 5");
+            int additon = Convert.ToInt32(Console.ReadLine()); //not in correct format, will only take one integer
+            int sum = Class1.method1(additon, additon);
+            Console.WriteLine(sum + " is the answer.");
+
+            //user input two numbers, one at a time
+            Console.WriteLine("Enter two numbers, one at a time. You dont't need to input a second number if you dont't want too.");
+            int subtraction = Convert.ToInt32(Console.ReadLine());
+            int difference = Class1.method2(subtraction, subtraction);
+            Console.WriteLine(difference + " is the answer.");
+
+            Console.ReadLine();
+        }
+    }
+}
